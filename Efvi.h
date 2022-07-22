6f2e76e0e51678672565641fab7a1a0b4ea2a016
Rao Meng <raomeng@localhost.localdomain>
2022-07-22 17:17:27 +0800

    Revert "stop using ef_vi_transmit_ctpio_fallback as it's wasting time"
    This reverts commit 9c4201e23a46e6a8d4e23e4419ff40e746a5168c.

--- Efvi.h
+++ Efvi.h
@@ -411,12 +411,13 @@ public:
     udp->udp_len_be16 = htons(8 + size);
     memcpy(pkt + 1, data, size);
     uint32_t frame_len = 42 + size;
-    int rc = 0;
+    int rc;
     if (use_ctpio) {
       uint32_t ipsum = ipsum_cache + iplen;
       ipsum += (ipsum >> 16u);
       ip4->ip_check_be16 = ~ipsum & 0xffff;
       ef_vi_transmit_ctpio(&vi, &pkt->eth, frame_len, 40);
+      rc = ef_vi_transmit_ctpio_fallback(&vi, pkt->post_addr, frame_len, buf_index_);
     }
     else {
       rc = ef_vi_transmit(&vi, pkt->post_addr, frame_len, buf_index_);
