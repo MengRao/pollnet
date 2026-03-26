c4ccd551148254a8f1ca99f05c84819c3706c623
Rao Meng <raomeng@localhost.localdomain>
2026-03-26 11:21:12 +0800

    minor change

--- example/build.sh
+++ example/build.sh
@@ -1,14 +1,15 @@
 set -e
 
 #TCPDIRECT_OPT="-DUSE_TCPDIRECT -lonload_zf"
-EFVI_OPT="-DUSE_EFVI -lciul1"
+#EFVI_OPT="-DUSE_EFVI -lciul1"
 
-#g++ -O3 -Wall tcpclient.cc -o tcpclient $TCPDIRECT_OPT $EFVI_OPT 
-#g++ -O3 -Wall tcpserver.cc -o tcpserver $TCPDIRECT_OPT $EFVI_OPT
+g++ -O3 -Wall tcpclient.cc -o tcpclient $TCPDIRECT_OPT $EFVI_OPT 
+g++ -O3 -Wall tcpserver.cc -o tcpserver $TCPDIRECT_OPT $EFVI_OPT
 #g++ -O3 -Wall udpreceiver.cc -o udpreceiver $EFVI_OPT
+#g++ -O3 -Wall udpreceiver.cc -o udpreceiver_efvi $EFVI_OPT
 #g++ -O3 -Wall tcpsniffer.cc -o tcpsniffer $EFVI_OPT
-g++ -O3 -Wall udpping.cc -o udpping 
-g++ -O3 -Wall udppong.cc -o udppong
+#g++ -O3 -Wall udpping.cc -o udpping 
+#g++ -O3 -Wall udppong.cc -o udppong
 #g++ -O3 -Wall udpping.cc -o udpping_efvi $EFVI_OPT
 #g++ -O3 -Wall udppong.cc -o udppong_efvi $EFVI_OPT
 #g++ -O3 -Wall udprecv.cc -o udprecv $EFVI_OPT
